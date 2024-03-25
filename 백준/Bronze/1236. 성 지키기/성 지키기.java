01619562577d81452cff03a3528a0f9e7dd09283
YangHyunWook <126859790+idusnr1117@users.noreply.github.com>
2024-03-25 19:07:20 +0900

    Update 성 지키기.java

--- 백준/Bronze/1236. 성 지키기/성 지키기.java
+++ 백준/Bronze/1236. 성 지키기/성 지키기.java
@@ -7,53 +7,54 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         String[] sizes = br.readLine().split(" ");
-        
         int n = Integer.parseInt(sizes[0]);
         int m = Integer.parseInt(sizes[1]);
 
         char[][] castle = new char[n][m];
 
-        for (int i = 0; i < n; i++)
+        for (int i = 0; i < n; i++) 
         {
-            castle[i] = br.readLine().toCharArray();
+            String line = br.readLine();
+            for (int j = 0; j < m; j++)
+            {
+                castle[i][j] = line.charAt(j);
+            }
         }
 
-        int rowCnt = 0;
-        int colCnt = 0;
+        int row_cnt = 0;
+        int col_cnt = 0;
 
         for (int i = 0; i < n; i++)
         {
-            boolean needGuardRow = true;
+            boolean need_guard = true;
             for (int j = 0; j < m; j++)
             {
                 if (castle[i][j] == 'X')
                 {
-                    needGuardRow = false;
+                    need_guard = false;
                     break;
                 }
             }
-            if (needGuardRow) rowCnt++;
+            if (need_guard) row_cnt++;
         }
 
         for (int j = 0; j < m; j++)
         {
-            boolean needGuardCol = true;
+            boolean need_guard = true;
             for (int i = 0; i < n; i++)
             {
                 if (castle[i][j] == 'X')
                 {
-                    needGuardCol = false;
+                    need_guard = false;
                     break;
                 }
             }
-            
-            if (needGuardCol)
+            if (need_guard)
             {
-            	colCnt++;
+            	col_cnt++;
             }
-            
         }
 
-        System.out.println(Math.max(rowCnt, colCnt));
+        System.out.println(Math.max(row_cnt, col_cnt));
     }
 }
