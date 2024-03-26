f9bd1c2b7a26dd6ed858ec80e16eb0bf9250ec82
YangHyunWook <126859790+idusnr1117@users.noreply.github.com>
2024-03-27 03:26:11 +0900

    [Bronze I] Title: 평균, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.py
+++ 백준/Bronze/1546. 평균/평균.py
@@ -1,13 +1,12 @@
-n = int(input())
-a = input().split()
-ans = 0
+N = int(input())
+A = input().split()
+B = 0
+for i in range(N):
+    A[i] = int(A[i])
 
-for i in range(n):
-    a[i] = int(a[i])
-
-max_score = max(a)
-for i in a:
-    i = i / max_score * 100
-    ans += i
-ans /= n
+max_score = max(A)
+for i in A:
+    i = (i/max_score) * 100
+    B += i
+ans = B / N
 print(ans)
