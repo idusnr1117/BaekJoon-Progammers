febf4ec15d7ff77a160db528d72408c1082d9e7b
YangHyunWook <126859790+idusnr1117@users.noreply.github.com>
2024-03-31 14:11:32 +0900

    [Bronze V] Title: 직사각형, Time: 124 ms, Memory: 14212 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		
+		int a = Integer.parseInt(bf.readLine());
+		int b = Integer.parseInt(bf.readLine());
+		
+		System.out.println(a * b);
+	}
+}
