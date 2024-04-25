11e6bdc8cbd813989eb79264f878607d9a3ca494
YangHyunWook <126859790+idusnr1117@users.noreply.github.com>
2024-04-25 10:24:25 +0900

    Update A×B.java

--- 백준/Bronze/10998. A×B/A×B.java
+++ 백준/Bronze/10998. A×B/A×B.java
@@ -3,14 +3,13 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-        int num1,num2;
 		
-		Scanner s = new Scanner(System.in);
-		num1 = s.nextInt();
-		num2 = s.nextInt();
+	Scanner sc = new Scanner(System.in);
+		
+	int num1 = sc.nextInt();
+	int num2 = sc.nextInt();
 
-		System.out.println(num1*num2);
+	System.out.println(num1 * num2);
 		
 	}
-
 }
