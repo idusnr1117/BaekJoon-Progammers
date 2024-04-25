ec79ef63c85e3b9b9994b5c8b99751aa0f34ce01
YangHyunWook <126859790+idusnr1117@users.noreply.github.com>
2024-04-25 10:22:46 +0900

    [Bronze V] Title: A×B, Time: 208 ms, Memory: 17700 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+        int num1,num2;
+		
+		Scanner s = new Scanner(System.in);
+		num1 = s.nextInt();
+		num2 = s.nextInt();
+
+		System.out.println(num1*num2);
+		
+	}
+
+}
