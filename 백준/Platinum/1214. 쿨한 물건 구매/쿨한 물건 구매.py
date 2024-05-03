b7b0c1b72393680779063e5a4fae502eafb9283d
YangHyunWook <126859790+idusnr1117@users.noreply.github.com>
2024-05-03 11:02:55 +0900

    Update 쿨한 물건 구매.py

--- 백준/Platinum/1214. 쿨한 물건 구매/쿨한 물건 구매.py
+++ 백준/Platinum/1214. 쿨한 물건 구매/쿨한 물건 구매.py
@@ -2,7 +2,7 @@ d, p, q = map(int,input().split())
 p, q = max(p, q),min(p, q)
 small = 1e10
 
-for i in range(min(d // p, q)+1):
+for i in range(min(d // p, q) + 1):
   small = min(small, (q - (d - p * i) % q) % q)
 
 small = min(small, (p - (d % p)) % p)
