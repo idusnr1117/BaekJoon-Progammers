00c3ac2df420f5b1bdcb347b1283600b166f3f12
YangHyunWook <126859790+idusnr1117@users.noreply.github.com>
2024-05-12 17:55:59 +0900

    [Bronze V] Title: 시험 성적, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,11 @@
+Score = int(input())
+if Score >= 90:
+    print('A')
+elif Score >= 80:
+    print('B')
+elif Score >= 70:
+    print('C')
+elif Score >= 60:
+    print('D')
+elif Score >= 0:
+    print('F')
