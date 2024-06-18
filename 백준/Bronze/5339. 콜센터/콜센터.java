d574ce468c8bfaff61d1d8aae4c91e009cb95c99
YangHyunWook <126859790+idusnr1117@users.noreply.github.com>
2024-06-18 10:07:55 +0900

    [Bronze V] Title: 콜센터, Time: 108 ms, Memory: 13852 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main
+{
+    public static void main(String[] args) throws IOException {
+
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        System.out.println(
+                "     /~\\\n" +
+                "    ( oo|\n" +
+                "    _\\=/_\n" +
+                "   /  _  \\\n" +
+                "  //|/.\\|\\\\\n" +
+                " ||  \\ /  ||\n" +
+                "============\n" +
+                "|          |\n" +
+                "|          |\n" +
+                "|          |"
+        );
+
+    }
+}
