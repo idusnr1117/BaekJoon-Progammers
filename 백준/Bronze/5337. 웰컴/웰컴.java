2e6d5d26f026a295dd39432fedd58f32c8a39dc8
YangHyunWook <126859790+idusnr1117@users.noreply.github.com>
2024-06-18 10:03:49 +0900

    [Bronze V] Title: 웰컴, Time: 104 ms, Memory: 13744 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Main
+{
+    public static void main(String[] args) throws IOException {
+
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        System.out.println(
+                ".  .   .\n" +
+                "|  | _ | _. _ ._ _  _\n" +
+                "|/\\|(/.|(_.(_)[ | )(/."
+        );
+
+    }
+}
