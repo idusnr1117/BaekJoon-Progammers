2a639f6c449fb6b3be28cffbad2bcd8de222b856
YangHyunWook <126859790+idusnr1117@users.noreply.github.com>
2024-06-18 10:39:01 +0900

    [Bronze V] Title: Zadanie próbne, Time: 132 ms, Memory: 16168 KB -BaekjoonHub

---
+++ 백준/Bronze/8545. Zadanie próbne/Zadanie próbne.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        String str = br.readLine();
+        String answer = "";
+        for (int i = str.length() - 1; -1 < i; i--)
+        {
+            answer += str.charAt(i);
+        }
+
+        System.out.println(answer);
+
+    }
+}
