8387619264e54936baee02f13a0a589c4e90301b
YangHyunWook <126859790+idusnr1117@users.noreply.github.com>
2024-06-19 19:52:56 +0900

    [Bronze V] Title: 대소문자 바꾸기, Time: 112 ms, Memory: 14172 KB -BaekjoonHub

--- 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.java
+++ 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.java
@@ -1,25 +1,28 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-	
-	public static void main(String[] args) {
-		
-	Scanner sc = new Scanner(System.in);
-	
-	String word[] = sc.nextLine().split("");
-	String result = "";
-	
-	for (int i = 0; i < word.length; i++) {
-		if (word[i].equals(word[i].toUpperCase()))
-		{
-			result += word[i].toLowerCase();
-		}
-		else
-		{
-			result += word[i].toUpperCase();
-		}
-	}
 
-	System.out.println(result);
-	}
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char[] chars = br.readLine().toCharArray();
+        StringBuilder result = new StringBuilder();
+
+        for (char ch : chars)
+        {
+            if (Character.isUpperCase(ch))
+            {
+                result.append(Character.toLowerCase(ch));
+            }
+            else
+            {
+                result.append(Character.toUpperCase(ch));
+            }
+        }
+
+        System.out.println(result);
+
+    }
 }
