4c94fe856cfde8f2569d95b8bd032c99ca2ec75d
YangHyunWook <126859790+idusnr1117@users.noreply.github.com>
2024-06-19 19:55:23 +0900

    [Silver V] Title: 문서 검색, Time: 124 ms, Memory: 14156 KB -BaekjoonHub

--- 백준/Silver/1543. 문서 검색/문서 검색.java
+++ 백준/Silver/1543. 문서 검색/문서 검색.java
@@ -1,19 +1,26 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-	
-    public static void main(String[] agrs) throws IOException {
-    	
+
+    public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         String word1 = br.readLine();
         String word2 = br.readLine();
+
+        int cnt = 0;
+        int index = word1.indexOf(word2);
+
+        while (index != -1)
+        {
+            cnt++;
+            index = word1.indexOf(word2, index + word2.length());
+        }
+
+        System.out.println(cnt);
         
-        int len1 = word1.length();
-		int len2 = word2.length();
-		
-		word1 = word1.replace(word2, "");
-		
-		System.out.println((len1 - word1.length()) / len2);
-        
-    }    
+    }
 }
