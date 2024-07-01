eb9dc32aef5f48251b5a11c770421ba45d55ac95
YangHyunWook <126859790+idusnr1117@users.noreply.github.com>
2024-07-01 14:05:15 +0900

    [Bronze III] Title: 별 찍기 - 6, Time: 108 ms, Memory: 14464 KB -BaekjoonHub

---
+++ 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine()) * 2;
+
+        for (int i = N - 1; 0 < i; i-=2)
+        {
+            System.out.print(" ".repeat((N - i) / 2));
+            System.out.println("*".repeat(i));
+        }
+
+    }
+}
