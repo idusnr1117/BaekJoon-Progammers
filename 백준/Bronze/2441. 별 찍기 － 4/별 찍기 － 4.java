bb98347100b7c3999b94b551c5d72163dd6c0955
YangHyunWook <126859790+idusnr1117@users.noreply.github.com>
2024-07-01 14:01:21 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 112 ms, Memory: 14524 KB -BaekjoonHub

---
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = N; 0 < i; i--)
+        {
+            System.out.print(" ".repeat(N - i));
+            System.out.println("*".repeat(i));
+        }
+
+    }
+}
