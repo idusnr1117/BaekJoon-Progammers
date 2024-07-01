adf8f338e4bcdd00324773fb45ce231313e8b247
YangHyunWook <126859790+idusnr1117@users.noreply.github.com>
2024-07-01 13:59:14 +0900

    [Bronze III] Title: 별 찍기 - 9, Time: 120 ms, Memory: 14532 KB -BaekjoonHub

---
+++ 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int first = N * 2 - 1;
+
+        for (int i = first; 0 < i; i-=2)
+        {
+            System.out.print(" ".repeat((Math.abs(first - i)) / 2));
+            System.out.println("*".repeat(i));
+        }
+
+        for (int i = 3; i <= first; i+=2)
+        {
+            System.out.print(" ".repeat((Math.abs(first - i)) / 2));
+            System.out.println("*".repeat(i));
+        }
+
+    }
+}
