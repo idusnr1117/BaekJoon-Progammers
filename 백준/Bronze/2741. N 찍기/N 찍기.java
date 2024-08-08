fc738d07b287e91bf598d9b323ce4add415a733a
YangHyunWook <126859790+idusnr1117@users.noreply.github.com>
2024-08-08 10:06:34 +0900

    [Bronze V] Title: N 찍기, Time: 708 ms, Memory: 29548 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+//        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i = 1; i <= N; i++)
+        {
+            System.out.println(i);
+        }
+
+
+    }
+}
