2841ad5f23daf6bc1ef1a4b8410de68f4f831bc9
YangHyunWook <126859790+idusnr1117@users.noreply.github.com>
2024-08-08 10:05:38 +0900

    [Bronze V] Title: 검증수, Time: 100 ms, Memory: 14156 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        double sum = 0;
+
+        for (int i = 0; i < 5; i++)
+        {
+            sum += Math.pow(Integer.parseInt(st.nextToken()), 2);
+        }
+
+        System.out.println((int) sum % 10);
+
+    }
+}
