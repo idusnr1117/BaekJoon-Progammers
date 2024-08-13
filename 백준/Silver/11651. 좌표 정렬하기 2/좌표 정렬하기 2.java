7a7defd641472c00b3aab9e5f2bd5177f6d3ef0d
YangHyunWook <126859790+idusnr1117@users.noreply.github.com>
2024-08-13 10:28:42 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 1472 ms, Memory: 58652 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[][] points = new int[N][2];
+
+        for (int i = 0; i < N; i++)
+        {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            points[i][0] = Integer.parseInt(st.nextToken());
+            points[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(points, new Comparator<int[]>()
+        {
+            @Override
+            public int compare(int[] p1, int[] p2)
+            {
+                if (p1[1] != p2[1])
+                {
+                    return Integer.compare(p1[1], p2[1]); // y가 다르면 y끼리 비교
+                }
+                else
+                {
+                    return Integer.compare(p1[0], p2[0]); // y가 같으면 x끼리 비교
+                }
+            }
+        });
+
+        for (int[] point : points)
+        {
+            System.out.println(point[0] + " " + point[1]);
+        }
+
+
+
+    }
+}
