77c674dc2a7cd8acba5dbca1be9ba444003696ea
YangHyunWook <126859790+idusnr1117@users.noreply.github.com>
2024-08-21 08:54:55 +0900

    [Bronze V] Title: Site Score, Time: 104 ms, Memory: 14316 KB -BaekjoonHub

---
+++ 백준/Bronze/20254. Site Score/Site Score.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        System.out.println
+        (
+                (Integer.parseInt(st.nextToken()) * 56) +
+                (Integer.parseInt(st.nextToken()) * 24) +
+                (Integer.parseInt(st.nextToken()) * 14) +
+                (Integer.parseInt(st.nextToken()) * 6)
+        );
+
+    }
+}
