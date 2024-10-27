a4d394507f9e0b008d880b16e7fbcd682ef2a711
YangHyunWook <126859790+idusnr1117@users.noreply.github.com>
2024-10-28 06:40:18 +0900

    [Silver V] Title: 단어 나누기, Time: 148 ms, Memory: 17128 KB -BaekjoonHub

--- 백준/Silver/1251. 단어 나누기/단어 나누기.java
+++ 백준/Silver/1251. 단어 나누기/단어 나누기.java
@@ -7,29 +7,29 @@ public class Main {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-       String str = br.readLine();
-       String best = null;
+        String str = br.readLine();
+        String best = null;
 
-       int len = str.length();
+        int len = str.length();
 
-       for (int i = 1; i < len - 1; i++)
-       {
-           for (int j = i + 1; j < len; j++)
-           {
-               String start = str.substring(0, i);
-               String middle = str.substring(i, j);
-               String last = str.substring(j);
+        for (int i = 1; i < len - 1; i++)
+        {
+            for (int j = i + 1; j < len; j++)
+            {
+                String start = str.substring(0, i);
+                String middle = str.substring(i, j);
+                String last = str.substring(j);
 
-               String reversed = new StringBuilder(start).reverse().toString() +
-                       new StringBuilder(middle).reverse() +
-                       new StringBuilder(last).reverse();
+                String reversed = new StringBuilder(start).reverse().toString() +
+                    new StringBuilder(middle).reverse() +
+                    new StringBuilder(last).reverse();
 
-               if (best == null || reversed.compareTo(best) < 0)
-                   best = reversed;
-           }
-       }
+                if (best == null || reversed.compareTo(best) < 0)
+                    best = reversed;
+            }
+        }
 
-       System.out.println(best);
+        System.out.println(best);
 
     }
 }
