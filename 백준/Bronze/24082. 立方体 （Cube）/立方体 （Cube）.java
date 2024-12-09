4dc581023b5121759116dc0a0962cb2069fd0689
YangHyunWook <126859790+idusnr1117@users.noreply.github.com>
2024-12-09 12:24:45 +0900

    [Bronze V] Title: 立方体 (Cube), Time: 108 ms, Memory: 14116 KB -BaekjoonHub

---
+++ 백준/Bronze/24082. 立方体 （Cube）/立方体 （Cube）.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int X = Integer.parseInt(br.readLine());
+
+        System.out.println((int)Math.pow(X, 3));
+
+    }
+}
