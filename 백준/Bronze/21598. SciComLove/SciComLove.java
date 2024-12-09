0e68bbc3f1e8bfb790862404a7d33aded220d529
YangHyunWook <126859790+idusnr1117@users.noreply.github.com>
2024-12-09 12:20:41 +0900

    [Bronze V] Title: SciComLove, Time: 104 ms, Memory: 14056 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/21598. SciComLove/SciComLove.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        System.out.print("SciComLove\n".repeat(N));
+
+    }
+}
