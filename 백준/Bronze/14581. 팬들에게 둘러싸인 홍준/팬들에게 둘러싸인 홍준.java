688e8c6b63bd695c343d0588354d4c5e3dbad9dc
YangHyunWook <126859790+idusnr1117@users.noreply.github.com>
2024-12-09 11:38:22 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 108 ms, Memory: 14272 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String id = br.readLine();
+
+        System.out.println(":fan::fan::fan:");
+        System.out.printf(":fan::%s::fan:\n", id);
+        System.out.println(":fan::fan::fan:");
+
+    }
+}
