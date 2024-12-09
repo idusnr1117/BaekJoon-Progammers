05ca378c4eda96230a8ba72b0086ba6d95303a80
YangHyunWook <126859790+idusnr1117@users.noreply.github.com>
2024-12-09 11:33:29 +0900

    [Bronze V] Title: We love kriii, Time: 104 ms, Memory: 14124 KB -BaekjoonHub

---
+++ 백준/Bronze/10718. We love kriii/We love kriii.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = "강한친구 대한육군";
+
+        System.out.printf("%s\n%s\n", str, str);
+
+    }
+}
