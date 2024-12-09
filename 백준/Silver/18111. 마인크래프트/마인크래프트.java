b5f0ed1985b78495640a7cceff4c6ac50028b1fc
YangHyunWook <126859790+idusnr1117@users.noreply.github.com>
2024-12-09 11:08:45 +0900

    [Silver II] Title: 마인크래프트, Time: 672 ms, Memory: 35484 KB -BaekjoonHub

--- 백준/Silver/18111. 마인크래프트/마인크래프트.java
+++ 백준/Silver/18111. 마인크래프트/마인크래프트.java
@@ -1,7 +1,5 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
@@ -37,8 +35,8 @@ public class Main {
                 for (int j = 0; j < M; j++)
                 {
                     int diff = land[i][j] - height;
-                    if (diff > 0) // 블록을 제거
-                    {
+                    if (diff > 0)
+                    { // 블록을 제거
                         time += 2 * diff;
                         inventory += diff;
                     }
