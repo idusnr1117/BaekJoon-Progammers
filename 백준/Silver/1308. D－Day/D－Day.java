74a320efba393f063c52ae49aad604fb22b428bb
YangHyunWook <126859790+idusnr1117@users.noreply.github.com>
2025-03-13 11:13:35 +0900

    [Silver V] Title: D-Day, Time: 108 ms, Memory: 14552 KB -BaekjoonHub

---
+++ 백준/Silver/1308. D－Day/D－Day.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.StringTokenizer;
+
+public class Main {
+    
+    public static void main(String[] args) throws IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int tYear = Integer.parseInt(st.nextToken());
+        int tMonth = Integer.parseInt(st.nextToken());
+        int tDay = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        int DYear = Integer.parseInt(st.nextToken());
+        int DMonth = Integer.parseInt(st.nextToken());
+        int DDay = Integer.parseInt(st.nextToken());
+
+        LocalDate today = LocalDate.of(tYear, tMonth, tDay);
+        LocalDate dDay = LocalDate.of(DYear, DMonth, DDay);
+
+        LocalDate limitDay = today.plusYears(1000);
+
+        if (!dDay.isBefore(limitDay))
+        {
+            System.out.println("gg");
+            return;
+        }
+
+        long daysDifference = ChronoUnit.DAYS.between(today, dDay);
+
+        System.out.printf("D-%d\n", daysDifference);
+    }
+}
