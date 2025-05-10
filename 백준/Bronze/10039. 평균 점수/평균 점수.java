f5be2ba671abde9284d114f129a122293e15055e
YangHyunWook <126859790+idusnr1117@users.noreply.github.com>
2025-05-11 02:19:02 +0900

    [Bronze IV] Title: 평균 점수, Time: 100 ms, Memory: 14116 KB -BaekjoonHub

---
+++ 백준/Bronze/10039. 평균 점수/평균 점수.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int sum = 0;
+
+        for (int i = 0; i < 5; i++)
+        {
+            int num = Integer.parseInt(br.readLine());
+
+            if (num < 40)
+                sum += 40;
+            else
+                sum += num;
+        }
+
+        System.out.println(sum / 5);
+
+    }
+}
