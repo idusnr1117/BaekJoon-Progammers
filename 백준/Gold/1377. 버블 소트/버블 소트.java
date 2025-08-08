0ca71cbd47a961ab8c209f539096515c80d4702a
YangHyunWook <126859790+idusnr1117@users.noreply.github.com>
2025-08-09 00:52:15 +0900

    [Gold II] Title: 버블 소트, Time: 1524 ms, Memory: 84636 KB -BaekjoonHub

---
+++ 백준/Gold/1377. 버블 소트/버블 소트.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static class mData implements Comparable<mData>
+    {
+        int value;
+        int index;
+
+        public mData (int value, int index)
+        {
+            super();
+            this.value = value;
+            this.index = index;
+        }
+
+        @Override
+        public int compareTo(mData o)
+        {
+            return this.value - o.value;
+        }
+
+    }
+
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        mData[] A = new mData[N];
+
+        for (int i = 0; i < N; i++)
+            A[i] = new mData(Integer.parseInt(br.readLine()), i);
+
+        Arrays.sort(A);
+
+        int max = 0;
+
+        for (int i = 0; i < N; i++)
+        {
+            if (max < A[i].index - i)
+                max = A[i].index - i;
+        }
+
+        System.out.println(max + 1);
+
+    }
+}
