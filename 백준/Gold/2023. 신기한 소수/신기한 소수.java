52284102280ed6d89b6df478313e3a4af5c9b76b
YangHyunWook <126859790+idusnr1117@users.noreply.github.com>
2025-08-10 20:26:30 +0900

    [Gold V] Title: 신기한 소수, Time: 704 ms, Memory: 14132 KB -BaekjoonHub

---
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int N;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+
+        dfs(2, 1);
+        dfs(3, 1);
+        dfs(5, 1);
+        dfs(7, 1);
+
+    }
+
+    static void dfs (int number, int jaritsu)
+    {
+        if (jaritsu == N)
+        {
+            if (isPrime(number))
+                System.out.println(number);
+            return;
+        }
+
+        for (int i = 1; i < 10; i++)
+        {
+            if (i % 2 == 0)
+                continue;
+
+            if (isPrime(number * 10 + i))
+                dfs(number * 10 + i , jaritsu + 1);
+        }
+    }
+
+    static boolean isPrime(int num)
+    {
+        for (int i = 2; i <= num / 2; i++)
+            if (num % i == 0)
+                return false;
+        return true;
+    }
+
+}
